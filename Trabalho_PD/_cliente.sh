3df54e6db8a78fc87a4d32d2a28ba3e90f6eb195
Hugo Pereira <80553701+Hugomjmp@users.noreply.github.com>
2024-10-13 14:26:05 +0100

    linux sh's

---
+++ Trabalho_PD/_cliente.sh
@@ -0,0 +1,1 @@
+java -cp . src/pt/isec/pd/cliente/Cliente.java 127.0.0.1 123
---
+++ Trabalho_PD/_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo java -cp "./out:sqlite-jdbc-3.46.1.3.jar" src/pt/isec/pd/servidores/ServidorPrincipal.java 123 Base_de_dados
