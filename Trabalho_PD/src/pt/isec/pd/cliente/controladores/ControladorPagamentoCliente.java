d488e5fd7c6a2104709ef1c92489398012bb84a3
DiogoCoelho0000 <74797729+DiogoCoelho0000@users.noreply.github.com>
2024-10-24 02:34:45 +0100

    .
    .

--- Trabalho_PD/src/pt/isec/pd/cliente/controladores/ControladorPagamentoCliente.java
+++ Trabalho_PD/src/pt/isec/pd/cliente/controladores/ControladorPagamentoCliente.java
@@ -7,15 +7,27 @@ import pt.isec.pd.comum.modelos.mensagens.InserePagamento;
 
 public class ControladorPagamentoCliente {
 
-    public static void inserirPagamento(Ligacao ligacao, String pagaPor, String recebidoPor, String grupoNome, double valor, String dataPagamentoStr) {
+    public static void inserirPagamento(Ligacao ligacao, String emailLogado, String grupoSelecionado, String pagaPor, String recebidoPor, double valor, String data) {
 
         if (valor <= 0) {
             System.out.println("Valor deve ser positivo.");
             return;
         }
-        InserePagamento pagamento = new InserePagamento(grupoNome, pagaPor, recebidoPor, valor);
+        InserePagamento pagamento = new InserePagamento(grupoSelecionado, pagaPor, recebidoPor, valor, data);
         Mensagem mensagem = new Mensagem(Tipomensagemenum.USER_INSERE_PAGAMENTO, pagamento);
         ligacao.enviaMensagem(mensagem);
         System.out.println("Pagamento enviado: " + mensagem);
     }
+
+    public static void eliminaPagamento(Ligacao ligacao, String emailLogado, String grupoSelecionado, String paymentId) {
+        if (paymentId == null || paymentId.isEmpty()) {
+            System.out.println("ID do pagamento deve ser fornecido.");
+            return;
+        }
+        EliminaPagamento elpagamento = new EliminaPagamento(paymentId, grupoSelecionado);
+        Mensagem mensagem = new Mensagem(Tipomensagemenum.USER_ELIMINA_PAGAMENTO_EFECTUADO, elpagamento);
+        ligacao.enviaMensagem(mensagem);
+        System.out.println("Pagamento eliminado: " + mensagem);
+    }
+
 }
